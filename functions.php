76a46d35232eaf1e47d22433180fc1c885a0682d
Daniel-Digid <Daniel-Digid@users.noreply.github.com>
2022-01-26 14:08:55 +0000

    Main Menu

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
-wp_enqueue_style( 'slider', get_template_directory_uri() . '/style.css',false,'1.1','all');
+add_action( 'after_setup_theme', 'kreuzbern_setup' );
+function kreuzbern_setup() {
+
+register_nav_menus( array(
+			'primary' => __( 'Primary Menu', 'kreuzbern' ),
+		) );
+
+add_theme_support( 'menus' );
+}
+wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css',false,'1.1','all');
 
 function nd_dosth_theme_setup() {
-
-        // Adds <title> tag support
-        add_theme_support( 'title-tag' );
-
+  add_theme_support( 'title-tag' );
 }
 add_action( 'after_setup_theme', 'nd_dosth_theme_setup');
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
