e60d948adb14ebc4c78ee5dc594cb15fc56589eb
Daniel-Digid <Daniel-Digid@users.noreply.github.com>
2022-02-01 11:25:43 +0000

    Latest changes - home structure

--- home-template.php
+++ home-template.php
@@ -19,6 +19,29 @@
                     // End of the loop.
     endwhile;
     ?>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9 col-sm-9 col-xs-12 main__section">
+            <div class="main__section--wrapper">
+              <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6"></div>
+              </div>
+              <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-4 col-xs-12 col-md-offset-1 sidebar float__right">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
   </main><!-- .site-main -->
 </div><!-- .content-area -->
 <?php get_footer(); ?>
