8aa666adc83e58b7a2f4c9cf42fd02f8618018ff
Pedro Silva <pedro@dig.id>
2022-02-02 17:11:09 +0000

    up

--- template-parts/sidebar-blocks.php
+++ template-parts/sidebar-blocks.php
@@ -3,13 +3,13 @@
 $sidebar_blocks = get_field( 'sidebar_blocks' );
 
 if ( $sidebar_blocks ) :
-	foreach ( $sidebar_blocks as $block ) :
+	foreach ( $sidebar_blocks as $post ) :
 		// Setup this post for WP functions (variable must be named $post).
-		setup_postdata( $block );
-		$block_template = the_field( 'block_template' );
-		/*if () :
-
-		endif;*/
+		setup_postdata( $post );
+		$block_template = get_field( 'block_template' );
+		if ( $block_template === 'light') :
+			echo 'light';
+		endif;
 		?>
 			<div class="block">
 				<h3 class="block__title"><?php the_title(); ?></h3>
@@ -17,7 +17,7 @@ if ( $sidebar_blocks ) :
 				<a class="block__btn" href="<?php the_field( 'block_button_url' ); ?>"><?php the_field( 'block_button_text' ); ?></a>
 			</div>
 		<?php
-	endforeach; 
+	endforeach;
 	// Reset the global post object so that the rest of the page works correctly.
 	wp_reset_postdata();
 endif;
