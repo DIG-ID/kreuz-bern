7631826c512e3a3fa8375d7d640f0c44f7690fbd
Pedro Silva <pedro@dig.id>
2022-02-04 16:23:43 +0000

    up

--- single-offers.php
+++ single-offers.php
@@ -22,7 +22,7 @@ get_header(); ?>
 									?>
 									<div class="block-image__content">
 										<h1 class="block-image__title"><?php the_title(); ?></h1>
-										<p class="block-image__text"><?php the_content(); ?></p>
+										<div class="block-image__text"><?php the_content(); ?></div>
 										<div class="block-image__btn--wrapper">
 											<a class="block-image__btn" href="<?php ?>"><?php esc_html_e( 'ZURÜCK ZUR ÜBERSICHT', 'kreuz-bern' ); ?></a>
 											<a class="block-image__btn block-image__btn--dark" href="<?php the_field( 'book_button_link' ); ?>"><?php the_field( 'book_button' ); ?></a>
