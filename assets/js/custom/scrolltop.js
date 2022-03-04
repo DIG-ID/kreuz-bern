22fb39b52ea746856ab71714d24bdb7e19223bbd
Pedro Silva <pedro@dig.id>
2022-03-04 17:00:03 +0000

    up

--- assets/js/custom/scrolltop.js
+++ assets/js/custom/scrolltop.js
@@ -1,18 +1,10 @@
 import $ from "jquery";
 
-// Scroll To Top - START CODE
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 100) {
-    $('.scrollToTop').fadeIn();
-  } else {
-    $('.scrollToTop').fadeOut();
-  }
-});
-
-//Click event to scroll to top
-$('.scrollToTop').on( 'click', function () {
-  $('html, body').animate({
-    scrollTop: 0
-  }, 800);
-  return false;
+$(function() {
+  $('.scrollToTop').on( 'click', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 300);
+  });
 });
--- footer.php
+++ footer.php
@@ -13,16 +13,16 @@ defined( 'ABSPATH' ) || exit;
 			<div class="container">
 				<div class="row justify-content-between align-items-start">
 					<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 px-15 footer-block">
-						<?php echo wp_kses_post( get_theme_mod( 'block-1' ) ); ?>
+						<?php echo wp_kses_post( wpautop( get_theme_mod( 'block-1' ) ) ); ?>
 					</div>
 					<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-3 px-15 footer-block">
-						<?php echo wp_kses_post( get_theme_mod( 'block-2' ) ); ?>
+						<?php echo wp_kses_post( wpautop( get_theme_mod( 'block-2' ) ) ); ?>
 					</div>
 					<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-2 px-15 footer-block">
-						<?php echo wp_kses_post( get_theme_mod( 'block-3' ) ); ?>
+						<?php echo wp_kses_post( wpautop( get_theme_mod( 'block-3' ) ) ); ?>
 					</div>
 					<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-2 px-15 footer-block">
-						<?php echo wp_kses_post( get_theme_mod( 'block-4' ) ); ?>
+						<?php echo wp_kses_post( wpautop( get_theme_mod( 'block-4' ) ) ); ?>
 					</div>
 					<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-2 px-15 footer-block">
 						<span class="footer-block__title"><?php esc_html_e( 'Follow us', 'kreuz-bern' ); ?></span>
@@ -36,9 +36,9 @@ defined( 'ABSPATH' ) || exit;
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-1">
-						<a class="scrollToTop" href="javascript:void(0);">
+						<div class="scrollToTop">
 							<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-up" class="svg-inline--fa fa-chevron-up fa-w-14 back-to-top" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#fff" d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>
-						</a>
+						</div>
 					</div>
 				</div>
 			</div><!-- container end -->
