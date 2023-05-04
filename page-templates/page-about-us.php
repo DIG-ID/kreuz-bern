cedad8b249fb99f5bb654ccf6d6c7a980e0f4320
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-05-04 15:56:37 +0100

    up

--- page-templates/page-about-us.php
+++ page-templates/page-about-us.php
@@ -10,6 +10,7 @@ get_header(); ?>
 	<?php do_action( 'wrapper_open' ); ?>
 	<?php
 		$sponsor_link  = get_field( 'sponsor_button' );
+		$page_image    = get_field( 'page_image' );
 		$history_image = get_field( 'history_block_image' );
 		$team_image    = get_field( 'team_block_image' );
 		$sponsor_image = get_field( 'sponsor_image' );
@@ -20,12 +21,25 @@ get_header(); ?>
 		</div>
 		<?php do_action( 'before_main_content' ); ?>
 			<div class="row">
-				<div class="col-12 col-sm-12 col-md-12 col-lg-6 px-15">
+				<div class="col-12 col-sm-12 col-md-12 col-lg-6 px-15 block-fh">
 					<div class="block block-wide">
 						<h2 class="block__title"><?php the_field( 'page_title' ); ?></h2>
 						<p class="block__text"><?php the_field( 'page_description' ); ?></p>
 					</div>
 				</div>
+				<!-- <div class="col-12 col-sm-12 col-md-12 col-lg-6 px-15">
+					<div class="block-image">
+						<?php
+						if ( $page_image  ) :
+							echo wp_get_attachment_image( $page_image , $size );
+						endif;
+						?>
+						<div class="block-image__content">
+							<h2 class="block-image__title"><?php the_field( 'page_title' ); ?></h2>
+							<p class="block-image__text"><?php the_field( 'page_description' ); ?></p>
+						</div>
+					</div><!-- block-image -->
+				<!-- </div><!-- .col --> 
 				<div class="col-12 col-sm-12 col-md-12 col-lg-6 px-15">
 					<div class="block-image">
 						<?php
