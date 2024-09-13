46f0e8a68a915bbf143f2b89a46f8ea8adb28352
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-09-13 13:17:23 +0100

    up

--- header.php
+++ header.php
@@ -18,7 +18,22 @@
 		<!-- Google Tag Manager -->
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K6NMG4G6');</script>
 		<!-- End Google Tag Manager -->
-
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-X4WP8TJ1PD"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('set', {
+					'linker': {
+							'accept_incoming': true,
+							'domains': ['www.simplebooking.it']
+					},
+					'anonymize_ip': true,
+							'transport_type': 'beacon'
+			});
+			gtag('config', 'UA-110180671-1');
+			gtag('config', 'G-X4WP8TJ1PD');
+		</script>
 		<!-- GLOBONET Tracking Code GA4 -->
 		<script>
 			var _paq = _paq || [];
