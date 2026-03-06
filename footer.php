7bb41e3543aaf272fc67eabb70a95ecca0cccbe3
Daniel-Digid <daniel@dig.id>
2026-03-06 12:26:23 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@ defined( 'ABSPATH' ) || exit;
 						<img src="<?php echo get_template_directory_uri(); ?>/src/images/Swisstainable_3_leading_RGB.png" style="max-width: 100px;">
 					</div>
 					<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-2 px-15 footer-block">
-					<img src="<?php echo get_template_directory_uri(); ?>/src/images/Ibex-BRONZE_rgb.jpg" style="max-width: 200px;">
+					<img src="<?php echo get_template_directory_uri(); ?>/src/images/Ibex-BRONZE_rgb.jpg" style="max-width: 180px;">
 					</div>
 				</div><!-- row end -->
 				<div class="row align-items-center justify-content-center d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxxl-none">
